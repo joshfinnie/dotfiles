1c03610f4d897d2f7f32b4415e766b427cae1685
Josh Finnie <josh@jfin.us>
2022-05-06 10:25:40 -0400

    doom dotfiles

---
+++ doom/config.el
@@ -0,0 +1,90 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Josh Finnie"
+      user-mail-address "josh@jfin.us")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "DroidSansMono Nerd Font" :size 12)
+      doom-variable-pitch-font (font-spec :family "DroidSansMono Nerd Font" :size 12))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-dracula)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; Setting neotree to use the icon font permentantly
+(setq neo-theme (if (display-graphic-p) 'icons 'icons))
+
+;; setup auto-save and backups
+(setq auto-save-default t
+      make-backup-files t)
+
+;; Disable exit confirmation
+(setq confirm-kill-emacs nil)
+
+;; Maximize window on open
+(setq initial-frame-alist '((top . 1) (left . 1) (width . 114) (height . 64)))
+
+;; set wordcount
+(setq doom-modeline-enable-word-count t)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(setq auto-mode-alist 
+    (append '((".*\\.tsx\\'" . typescript-mode))
+        auto-mode-alist))
+
+(setq auto-mode-alist 
+    (append '((".*\\.astro\\'" . js-jsx-mode))
+        auto-mode-alist))
+
+(after! typescript-mode
+    (add-hook 'typescript-mode-hook #'flycheck-mode))
+
+(setq rustic-lsp-server 'rust-analyzer)
+
+(map! :leader
+      :desc "Open treemacs"
+      "n n" #'treemacs)
+
+(after! dtrt-indent
+    (add-to-list 'dtrt-indent-hook-mapping-list '(typescript-mode javascript typescript-indent-level)))
+
---
+++ doom/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(markdown-list-indent-width 2)
+ '(package-selected-packages '(key-chord)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
